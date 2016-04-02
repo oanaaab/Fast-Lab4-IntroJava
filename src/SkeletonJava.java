b339617b76d076c800185faf50e47649daa96a65
Oana Balint <oanab95@gmail.com>
2016-04-02 13:17:24 +0300

    lab 4

--- src/SkeletonJava.java
+++ src/SkeletonJava.java
@@ -75,12 +75,155 @@ public class SkeletonJava {
 
     /* here starts the main class */
     public static void main(String[] arguments) {
-        int a = readIntGUI("Introduceti un numar:"); // input using GUI
+       /* int a = readIntGUI("Introduceti un numar:"); // input using GUI
         printGUI("Ati introdus:" + a); // output using GUI
 
         int b = readIntConsole("Introduceti alt numar:");  // input using console
-        printConsole("Ati introdus:" + b); // output using console
+        printConsole("Ati introdus:" + b); // output using console */
+
+
+        // EX 2
+
+        // n1, n2, media aritmetica, daca media e peste 5 ai trecut clasa, altfel corigent
+
+       /* int n1 = readIntConsole("Introduceti un numar:");
+        int n2 = readIntConsole("Introduceti alt numar:");
+        double medie = (n1 + n2) / 2.0;
+        if (medie <= 4) {
+            System.out.println("Din pacate, nu ai trecut clasa!");
+            System.out.println("Media ta este" +medie);
+        } else {
+            if (medie == 5 ) {
+                System.out.println("Ai trecut la limita! Painea elevului!");
+                System.out.println("Media ta este " +medie);
+            } else {
+                if (medie == 6 ) {
+            System.out.println("Asa si asa!");
+            System.out.println("Media ta este " +medie);}
+                else  {
+                    System.out.println("Bravo! Ai trecut clasa cu bine!");}
+                }
+
+        } */
+
+
+
+/* Se poate si asa
+        int n1 = readIntConsole("Introduceti un numar:");
+        int n2 = readIntConsole("Introduceti alt numar:");
+        int medie = (n1 + n2) / 2;
+       switch (medie) {
+           case 1:
+               printConsole("Ai picat!");
+               break;
+           case 2:
+               printConsole("Ai picat!");
+               break;
+           case 3:
+               printConsole("Ai picat!");
+               break;
+           case 4:
+               printConsole("Ai picat!");
+               break;
+           case 5:
+               printConsole("Ai trecut la limita!");
+               break;
+           case 6:
+               printConsole("Asa si asa!");
+               break;
+           case 7:
+               printConsole("Bravo!");
+               break;
+           default:
+               printConsole("Bravo!");
+       } */
+
+
+
+
+        /* --- //pentru a afisa nr de la 1 la 100, declaram contorul, punem conditia, si adunam contorul, printam contorul
+        for (int contor=1; contor<101; contor++) {
+            System.out.println(contor);
+        } */
+
+
+
+
+      /*  // Sa se citeasca numere pana se introduce 0
+int n; //declaram variabila
+        do
+        {
+            n = readIntConsole("Introduceti un numar:"); //citeste numarul introdus
+        }
+       // while( n != 0); //cere numere PANA CAND n este diferit de 0, atata timp cat n nu e 0 stai acolo...
+        while( n*2 < 100 ); // pana cand dublura nr trece de 100   */
+
+
+        //array cu medie
+        /*
+
+        //declaram array
+        int[] notemate = {5, 6, 2, 9};
+        int suma = 0;
+        //parcurgem sirul
+        for (int i = 0; i < notemate.length; i++)
+            System.out.println(notemate[i]);
+        suma = suma + notemate[i];
+        System.out.println("pas" + i + "suma=" + suma);
+
+    int media = suma/notemate.length
+            */
+
+        //note de la tastatura + medie
+      /*  int sum = 0;
+        int[] note=new int[2];
+        for (int i=0; i<note.length;i++) {
+            note[i] = readIntConsole("Introdu nota:");
+            sum += note[i];
+        }
+        int media = sum/note.length;
+        System.out.println("Media e: "+media);
+        */
+
+        // cu max!
+      /*  int sum = 0;
+        int[] note=new int[2];
+        for (int i=0; i<note.length;i++) {
+            note[i] = readIntConsole("Introdu nota:");
+            sum += note[i];
+        }
+        int max = note[0];
+        for (int i = 0; i <note.length; i++) {
+            if (note[i] > max) {
+                max =note[i];}
+            }
+        System.out.println("Nota maxima e: " +max);
+        */
+
+        // sa se calculeze numerele introduse pana cand dam 0
+        int n; //declaram variabila
+        int sum = 0;
+
+        do
+        {
+            n = readIntConsole("Introduceti un numar:");
+            sum += n
+
+        }
+        while( n != 0);
+
+
+
     }
+
+        }
+
+
+
+
+
+
+
     //end of main method
-}
+
 // end of class
